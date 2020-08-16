a8762c466e9d7f93010c305b49f1e9497da69e74
Sushmitha0805 <rsushmitha822@gmail.com>
2020-08-16 17:51:54 +0530

    added uc4 triplet

--- flipcoin.sh
+++ flipcoin.sh
@@ -82,3 +82,69 @@ TT=$(echo | awk '{print '$COUNT_3/$LENGTH_OF_ARRAY*100'}')
 
 Doublet=($HH $HT $TH $TT)
 echo ${Doublet[@]}
+
+#Flip coin for Triplet combination
+
+while [[ $NUMBER_TRIPLET -le 20 ]]
+do
+   FLIP_CHECK=$(( RANDOM%2))
+   FLIP_CHECK_ONE=$(( RANDOM%2))
+   FLIP_CHECK_TWO=$(( RANDOM%2))
+   if [[ $FLIP_CHECK -eq $HEAD && $FLIP_CHECK_ONE -eq $HEAD && $FLIP_CHECK_TWO -eq $HEAD ]]
+   then
+      COIN_FLIP="HHH"
+      COUNT_4=$(( $COUNT_4+1 ))
+	elif [[ $FLIP_CHECK -eq $HEAD && $FLIP_CHECK_ONE -eq $HEAD && $FLIP_CHECK_TWO -eq $TAIL ]]
+   then
+      COIN_FLIP="HHT"
+      COUNT_5=$(( $COUNT_5+1 ))
+	elif [[ $FLIP_CHECK -eq $HEAD && $FLIP_CHECK_ONE -eq $TAIL && $FLIP_CHECK_TWO -eq $HEAD ]]
+   then
+      COIN_FLIP="HTH"
+      COUNT_6=$(( $COUNT_6+1 ))
+	elif [[ $FLIP_CHECK -eq $TAIL && $FLIP_CHECK_ONE -eq $HEAD && $FLIP_CHECK_TWO -eq $HEAD ]]
+   then
+      COIN_FLIP="THH"
+      COUNT_7=$(( $COUNT_7+1 ))
+   elif [[ $FLIP_CHECK -eq $TAIL && $FLIP_CHECK_ONE -eq $TAIL && $FLIP_CHECK_TWO -eq $TAIL ]]
+   then
+      COIN_FLIP="TTT"
+      COUNT_8=$(( $COUNT_8+1 ))
+ 	elif [[ $FLIP_CHECK -eq $TAIL && $FLIP_CHECK_ONE -eq $TAIL && $FLIP_CHECK_TWO -eq $HEAD ]]
+   then
+      COIN_FLIP="TTH"
+      COUNT_9=$(( $COUNT_9+1 ))
+	elif [[ $FLIP_CHECK -eq $TAIL && $FLIP_CHECK_ONE -eq $HEAD && $FLIP_CHECK_TWO -eq $TAIL ]]
+   then
+      COIN_FLIP="THT"
+      COUNT_10=$(( $COUNT_10+1 ))
+	else [[ $FLIP_CHECK -eq $HEAD && $FLIP_CHECK_ONE -eq $TAIL && $FLIP_CHECK_TWO -eq $TAIL ]]
+      COIN_FLIP="HTT"
+      COUNT_11=$(( $COUNT_11+1 ))
+
+	fi
+   Triplet[$NUMBER_TRIPLET]=$COIN_FLIP
+   NUMBER_TRIPLET=$(( $NUMBER_TRIPLET+1 ))
+   echo ${Triplet[@]}
+done
+LENGTH_ARRAYT=${#Triplet[@]}
+
+HHH=$(echo | awk '{print '$COUNT_4/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination HHH " $HHH
+HHT=$(echo | awk '{print '$COUNT_5/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination HHH " $HHT
+HTH=$(echo | awk '{print '$COUNT_6/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination HTH " $HTH
+THH=$(echo | awk '{print '$COUNT_7/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination THH " $THH
+TTT=$(echo | awk '{print '$COUNT_8/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination TTT " $TTT
+TTH=$(echo | awk '{print '$COUNT_9/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination TTH " $TTH
+THT=$(echo | awk '{print '$COUNT_10/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination THT " $THT
+HTT=$(echo | awk '{print '$COUNT_11/$LENGTH_ARRAYT*100'}')
+	echo " Winning Percentage for Triplet Combination HHH " $HTT
+
+Triplet_Array=($HHH $HHT $HTH $THH $HTT $THT $TTH $TTT)
+echo ${Triplet_Array[@]}
